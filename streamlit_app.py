ea669302e00ae21b9ed5fcc31d12723869152af3
Georsx123 <geovanipradipta12345@gmail.com>
2024-11-26 17:11:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ if st.button("Hitung volume",type="primary"):
   loading = st.progress(0)
   for i in range(100):
     time.sleep(0.01)
-    loading.progress(i-1)
+    loading.progress(i+1)
   
   v = math.pi*(r**2)*t
   st.success(f"Volume tabung adalah {v:.2f}")
